7e62a0bb6886706120187f0bd34fc91f2601d29e
Gabriel Wachira <gabbyk.com@gmail.com>
2024-01-26 13:50:09 +0300

    update tsk 11 nosql

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -7,7 +7,7 @@ def top_students(mongo_collection):
     """Returns all students sorted by average score."""
     return mongo_collection.aggregate(
         [
-            {"$addFields": {"averageScore": {"$avg": "$scores.score"}}},
+            {"$addFields": {"averageScore": {"$avg": "$topics.score"}}},
             {"$sort": {"averageScore": -1}},
         ]
     )
