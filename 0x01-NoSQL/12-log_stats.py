69a69b5ba1c413e9c5558af41e00859c10cb4767
Gabriel Wachira <gabbyk.com@gmail.com>
2024-01-26 13:45:20 +0300

    add try blcok t0 tsk 12 nosql

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,28 +2,31 @@
 
 """Module for stats of nginx logs stored in mongodb."""
 if __name__ == "__main__":
-    import pymongo
+    try:
+        import pymongo
 
-    client = pymongo.MongoClient()
-    db = client["logs"]
-    coll = db["nginx"]
+        client = pymongo.MongoClient()
+        db = client["logs"]
+        coll = db["nginx"]
 
-    print(f"{coll.count_documents({})} logs")
-    print("Methods:")
-    print(
-        f"    method GET: {coll.count_documents({'method': 'GET'})}"
-        )
-    print(
-        f"    method POST: {coll.count_documents({'method': 'POST'})}"
-        )
-    print(
-        f"    method PUT: {coll.count_documents({'method': 'PUT'})}"
-        )
-    print(
-        f"    method PATCH: {coll.count_documents({'method': 'PATCH'})}"
-        )
-    print(
-        f"    method DELETE: {coll.count_documents({'method': 'DELETE'})}"
-        )
-    filter = coll.count_documents({'method': 'GET', 'path': '/status'})
-    print(f"{filter} status check")
+        print(f"{coll.count_documents({})} logs")
+        print("Methods:")
+        print(
+            f"    method GET: {coll.count_documents({'method': 'GET'})}"
+            )
+        print(
+            f"    method POST: {coll.count_documents({'method': 'POST'})}"
+            )
+        print(
+            f"    method PUT: {coll.count_documents({'method': 'PUT'})}"
+            )
+        print(
+            f"    method PATCH: {coll.count_documents({'method': 'PATCH'})}"
+            )
+        print(
+            f"    method DELETE: {coll.count_documents({'method': 'DELETE'})}"
+            )
+        filter = coll.count_documents({'method': 'GET', 'path': '/status'})
+        print(f"{filter} status check")
+    except Exception:
+        pass
